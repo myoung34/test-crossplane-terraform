a49183e4052b56971daa097b09caaa1133a50775
Marcus Young <myoung34@my.apsu.edu>
2022-12-28 11:15:11 -0600

    Add a test bucket for crossplane to peek

---
+++ bucket.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "us-marcyoung-crossplane"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_s3_bucket" "octopi" {
+  bucket = "us-marcyoung-sometest"
+  acl    = "private"
+
+  tags = {
+    Name       = "us-marcyoung-sometest"
+    managed_by = "terraform"
+  }
+
+  lifecycle_rule {
+    enabled                                = true
+    prefix                                 = ""
+    abort_incomplete_multipart_upload_days = 1
+
+    expiration {
+      days                         = 1
+      expired_object_delete_marker = true
+    }
+  }
+}
